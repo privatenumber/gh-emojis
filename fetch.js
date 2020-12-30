6ddc2bd4c70bdedd0b9947adeaf7fe5519ee1ae5
Hiroki Osame <hiroki.osame@gmail.com>
2020-12-30 17:45:02 -0500

    docs(readme): added emojis

--- fetch.js
+++ fetch.js
@@ -1,6 +1,7 @@
 const { promises: fs } = require('fs');
 const got = require('got');
 const { rollup } = require('rollup');
+const commentMark = require('comment-mark');
 
 (async () => {
 	const emojisJsonString = await got('https://api.github.com/emojis').text();
@@ -24,4 +25,27 @@ const { rollup } = require('rollup');
 			format: 'esm',
 		}),
 	]);
+
+	let mdString = await fs.readFile('./README.md');
+
+	mdString = commentMark(mdString, {
+		emojis: [
+			'| Name | Emoji | Name | Emoji |',
+			'| - | - | - | - |',
+			...((rows) => {
+				const emojis = Object.entries(JSON.parse(emojisJsonString));
+				const emojiColumns = ([name, url] = []) => [
+					name ? `\`${name}\`` : '',
+					url ? `<img width="20" src="${url}">` : '',
+				].join(' | ');
+
+				for (let i = 0; i < emojis.length; i += 2) {
+					rows.push(`| ${emojiColumns(emojis[i])} | ${emojiColumns(emojis[i + 1])} |`);
+				}
+				return rows;
+			})([]),
+		].join('\n'),
+	});
+
+	await fs.writeFile('./README.md', mdString);
 })();
